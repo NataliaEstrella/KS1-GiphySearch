541ff2103983f1a8e2ac081e44c35c99ad0a3298
Estrella, Natalia [GCB-OT] <ne82920@imcnam.ssmb.com>
2018-04-15 19:43:57 -0400

    Initial Commit

---
+++ ProjectKS1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectKS1
+//
+//  Created by Estrella, Natalia [GCB-OT] on 4/15/18.
+//  Copyright © 2018 Estrella, Natalia [GCB-OT]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
